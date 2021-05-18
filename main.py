983c2dd10ceca2e1ffb71d42ddd579e96266127b
Carlos-Arruda <ccfoxcolt2@gmail.com>
2021-05-18 01:13:13 -0300

    resolvi um pepino

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+
 from random import randint
 
 palavras = ('gato', 'Carro')
-opcao = randint(0,1)
+opcao = randint(0,len(palavras))
+jogo = palavras[opcao]
 
+print('A sua palavra foi selecionada')
+
+while True:    
+    for c in jogo:
+        print('-',end='')
+
+    letra = str(input('Digite uma letra'))
+
+
+print()
 print(palavras[opcao])
